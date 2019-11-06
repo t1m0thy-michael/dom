2e4d8aab7290c212b25a650744794b4264bd417d
timothy_michael <tim.everington@gmail.com>
2019-11-06 20:57:10 +0000

    selection methods get element as first arg (same as everything else)

--- src/methods/selection.ts
+++ src/methods/selection.ts
@@ -10,54 +10,54 @@ import { runAndReturnFactory } from '../utils/run'
 import { dom } from '../dom'
 
 // adds classes to the elements we're searching for then uses querySelectorAll
-export const child = (obj: DomObject, needle: DomInitiator | DomObject): DomObject => {
-	if (!obj.element|| !obj.element.querySelectorAll) return dom([])
+export const child = (element: DomElement, needle: DomInitiator | DomObject): DomObject => {
+	if (!element|| !element.querySelectorAll) return dom([])
 	const identifier = makeID(10,'_')
 	needle = dom(needle)
 	needle.addClass(identifier)
-	const result = dom(obj.element.querySelectorAll(`.${identifier}`))
+	const result = dom(element.querySelectorAll(`.${identifier}`))
 	needle.removeClass(identifier)
 	return result
 }
 
-export const sibling = (obj: DomObject, needle: DomInitiator | DomObject): DomObject => {
-	if (!obj.element.parentNode || !needle) return dom([])
+export const sibling = (element: DomElement, needle: DomInitiator | DomObject): DomObject => {
+	if (!element.parentNode || !needle) return dom([])
 	const queryResult = dom(needle).list
-	return dom(queryResult.filter((elem) => elem !== obj.element))
+	return dom(queryResult.filter((elem) => elem !== element))
 }
 
-export const parent = (obj: DomObject, needle: DomInitiator | DomObject): DomObject => {
-	if (!obj.element || !obj.element.closest) return dom([])
-	if (isString(needle)) return dom(obj.element.closest(needle))
+export const parent = (element: DomElement, needle: DomInitiator | DomObject): DomObject => {
+	if (!element || !element.closest) return dom([])
+	if (isString(needle)) return dom(element.closest(needle))
 
 	needle = dom(needle)
 	const results = [] as DomElement[]
 	needle.list.forEach((n:  DomElement) => {
-		if (dom(n).child(obj).list.length) results.push(n)
+		if (dom(n).child(element).list.length) results.push(n)
 	})
 	return dom(results)
 }
 
 export const isAppended = (element: NodeDescendant): boolean => document.body.contains(element)
 
-export const is = (obj: DomObject, selector: string): DomObject => {
-	if (!obj.element.matches || !obj.element.matches(selector)) return dom([])
-	return dom(obj.element)
+export const is = (element: DomElement, selector: string): DomObject => {
+	if (!element.matches || !element.matches(selector)) return dom([])
+	return dom(element)
 }
 
-export const not = (obj: DomObject, selector: string): DomObject => {
-	if (obj.element.matches && obj.element.matches(selector)) return dom([])
-	return dom(obj.element)
+export const not = (element: DomElement, selector: string): DomObject => {
+	if (element.matches && element.matches(selector)) return dom([])
+	return dom(element)
 }
 
 // Returns single new DomObject created from result.list arrays
 // only for use with methods that always return DomObjects
-export const runAndReturnSingleDomObjectFactory = <T extends any[]>(fn: (o: DomObject, ...args: T) => DomObject): ((...args: T) => DomObject) =>
+export const runAndReturnSingleDomObjectFactory = <T extends any[]>(fn: (o: DomElement, ...args: T) => DomObject): ((...args: T) => DomObject) =>
 	function (this: DomObject, ...args) {
 		let results = []
 		for (let i = 0; i < this.list.length; i++) {
 			try {
-				results.push(...fn(dom(this.list[i]), ...args).list)
+				results.push(...fn(this.list[i], ...args).list)
 			} catch (e) {
 				console.error(e)
 			}
